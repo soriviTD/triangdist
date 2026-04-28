4f182ddc698babaa5a124ecc3e38e7a897a79e6c
soriviTD <sorivi@alumni.uv.es>
2026-04-28 14:38:30 +0200

    Corrección de errores. Documentación.

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -1,106 +1,115 @@
-#min: a (valor donde empieza el triángulo)
-#max: b (valor donde acaba el triángulo)
-#mode: c (valor en x donde está la punta del triángulo, es el más común)
+#min: a (value where the triangle starts)
+#max: b (value where the triangle finishes)
+#mode: c (value in x where is the top of the triangle, it is the most
+#common one)
 
-#Para calcular el área, es decir, la densidad, hay que dividir
-#el triángulo en dos subtriángulos. Trazando una línea vertical
-#desde la punta (c) hasta la base.
-#La altura está en: 2/(b-a)
+#To calculate the area, meaning, the density, the triangle must be divided in
+#two subtriangles (drawing a vertical line from c to the base).
+#Height is in: 2/(b-a)
+#The area of each triangle is calculated separately.
 
-#Se calcula el área de cada triángulo por separado.
+#' Triangular Distribution
+#'
+#'Density, distribution , quantile function and random generation for the
+#'triangular distribution with parameters min, max and mode.
+#'
+#' @param x,q Quantile.
+#' @param p Probability.
+#' @param n Observations to generate.
+#' @param min Minimum value.
+#' @param max Maximum value.
+#' @param mode Mode value.
+#' #'
+#' @name triang_dist
+NULL
 
-#(-b+-sqrt(b^2-4*a*c))/2*a
-
-#Fórmula para calcular una ecuación de segundo grado.
-res_g2 <- function(a, b, c){
-  x = NA
-  raiz = b^2-4*a*c
-  if (raiz > 0){
-    x1 <- -b+sqrt(raiz)/2*a
-    x2 <- -b-sqrt(raiz)/2*a
-    if (x1 >= 0){
-      x <- x1
-    } else if (x2 >= 0){
-      x <- x2
-    } else {
-      stop('El valor introducido no es válido.')
-    }
-  } else if (raiz == 0) {
-    x1 <- -b/2*a
-    if (x1 >= 0){
-      x <- x1
-    }
+#'Density function.
+#Height in a specific point.
+#' @rdname triang_dist
+#' @return \code{dtriang} Returns density value.
+#' @export
+dtriang <- function(x, min, max, mode) {
+  if (min >= max || mode < min || mode > max) {
+    stop("One or more values are wrong.")
   } else {
-    stop('El valor introducido no es válido.')
+    if (x == mode) {
+      val <- 2 / (max - min)
+    } else if (x < min || x > max) {
+      val <- 0
+    } else if (x < mode) {
+      val <- 2 * (x - min) / ((max - min) * (mode - min))
+    } else if (x > mode) {
+      val <- 2 * (max - x) / ((max - min) * (max - mode))
+    }
+    val
   }
-  return (x)
 }
 
-#Density function.
-#La altura en un punto exacto.
-dtriang <- function(x, min, max, mode){
-  if (x == mode) {
-    val = 2/(max-min)
-  } else if (x<min | x>max){
-    val = 0
-  } else if (x < mode){
-    val = 2*(x-min)/((max-min)*(mode-min))
-  } else if (x>mode){
-    val = 2*(max-x)/((max-min)*(max-mode))
-  }
-  return (val)
-}
-
-#Distribution function.
-#Área bajo la curva a la izquierda de un punto q (P(X<q)).
-ptriang <- function(q, min, max, mode){
-  h = 0
-  if (q == mode) {
-    h = 2/(max-min)
-  } else if (q<=min){
-    val = 0
-  } else if (q>=max){
-    val = 1
-  } else if (q < mode){
-    h = 2*(q-min)/((max-min)*(mode-min))
-    val = 0.5*h*q
-  } else if (q>mode){
-    h = 2*(max-q)/((max-min)*(max-mode))
-    val = 1-(max-q)*h*0.5
-  }
-  return (val)
-}
-
-#Quantile function.
-#Le das la probabilidad y te dice qué valor de x le corresponde (inversa de
-#ptriang).
-qtriang <- function(p, min, max, mode){
-  h_mode = 2/(max-min)
-  p_mode = ptriang(mode, min, max, mode)
-  if (p>1 | p<0){
-    stop('El valor introducido debe estar entre 0 y 1 incluidos.') #Error
+#'Distribution function.
+#P(X<q).
+#' @rdname triang_dist
+#' @return \code{ptriang} Returns cumulative density value.
+#' @export
+ptriang <- function(q, min, max, mode) {
+  if (min >= max || mode < min || mode > max) {
+    stop("One or more values are wrong.")
   } else {
-    if (p == p_mode){
-      val = mode
-    } else if (p < p_mode){
-      val <- res_g2(2, -4*min, min^2-(max-min)*(mode-min)*2*p)
+    h <- 0
+    if (q == mode) {
+      h <- 2 / (max - min)
+      val <- (mode - min) * h / 2
+    } else if (q <= min) {
+      val <- 0
+    } else if (q >= max) {
+      val <- 1
+    } else if (q < mode) {
+      h <- 2 * (q - min) / ((max - min) * (mode - min))
+      val <- 0.5 * h * (q - min)
+    } else if (q > mode) {
+      h <- 2 * (max - q) / ((max - min) * (max - mode))
+      val <- 1 - (max - q) * h * 0.5
+    }
+    val
+  }
+}
+
+#'Quantile function.
+#You give to it the probability and it tells you which x corresponds to it
+#(ptriang^-1).
+#' @rdname triang_dist
+#' @return \code{qtriang} Returns the quantile value.
+#' @export
+qtriang <- function(p, min, max, mode) {
+  if (min >= max || mode < min || mode > max || p > 1 || p < 0) {
+    stop("One or more values are wrong.")
+  } else {
+    p_mode <- ptriang(mode, min, max, mode)
+    if (p == p_mode) {
+      val <- mode
+    } else if (p < p_mode) {
+      val <- sqrt(p * (max - min) * (mode - min)) + min
     } else if (p > p_mode) {
-      aux <- res_g2(1, 2*max, max^2-(1-p)*(max-min)*(max-mode))
-      x <- max-aux
+      val <- -sqrt((1 - p) * (max - min) * (max - mode)) + max
     }
+    val
   }
 }
 
-#Random generation
-rtriang <- function(n, min, max, mode){
-  val <- runif(n, 0, 1)
-  val <- qtriang(val, min, max, mode)
-  return (val)
+#' Random generation.
+#' @rdname triang_dist
+#' @return Vector of length \code{n} with randomly generated values.
+#'
+#' @importFrom stats runif
+#' @export
+rtriang <- function(n, min, max, mode) {
+  if (any(min >= max || mode < min || mode > max)) {
+    stop("One or more values are wrong.")
+  } else {
+    aux <- runif(n, 0, 1)
+    val <- rep(NA, n)
+    for (i in seq_along(aux)) {
+      val[i] <- qtriang(aux[i], min, max, mode)
+    }
+    val
+  }
 }
-
-
-
-
-#Source/Reference (ver cuál)
-#https://www.youtube.com/watch?v=kYmx_h5ril0
-#Ayuda de R para runif function
